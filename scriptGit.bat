b33502031bf6c00683a30c353dbb90d177af97d9
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2024-11-06 19:38:49 -0300

    desafio do git

---
+++ scriptGit.bat
@@ -0,0 +1,10 @@
+cd /
+git clone https://github.com/LucasPires192/teste.git
+cd teste
+git config --global user.email "lucas.andrade192@etec.sp.gov.br"
+git config --global user.name "LucasPires192"
+hostname > hostname.txt
+ipconfig > ip.txt
+git add .
+git commit -m "textos de logs"
+git push
